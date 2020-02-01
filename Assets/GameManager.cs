40b38ba2ffb22a0c0a8ee1991862ef88ea7524bd
Enllwx <34975808+Enllwx@users.noreply.github.com>
2020-02-01 10:17:34 -0500

    some information

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// up-left   : x - 0.5, y + 0.25
+// up-right  : x + 0.5, y + 0.25
+// down-left : x - 0.5, y - 0.25
+// down-rigth: x + 0.5, y - 0.25
+// down      :          y - 0.25 
+// up        :          y + 0.25
+
+
+public class GameManager : MonoBehaviour
+{
+    
+    const float BLOCLSIZE = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
